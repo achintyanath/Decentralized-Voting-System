66e78277c0027bf0abc4426d01da2b29b7af65f6
Achintya Nath <achintya_n@cs.iitr.ac.in>
2022-04-16 19:57:34 +0530

    Add NotInitialised module

---
+++ client/src/componenets/Notinit/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NotInit (){
+  return (
+    <div className="container-item info">
+      <div stule={{textAlign:'center'}}>
+        <h2>The election has not yet been initialised.</h2>
+        <p>Please Wait for the admin to start the election</p>
+      </div>
+    </div>
+  );
+};
+export default NotInit;

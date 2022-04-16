3d639253eb90bf3a4e108260b5cf3c7f05dd5ced
Achintya Nath <achintya_n@cs.iitr.ac.in>
2022-04-17 02:47:50 +0530

    Fix Routes

--- client/App.js
+++ client/App.js
@@ -1,43 +1,37 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Home from './src/componenets/Home/Home'
+import Voting from './src/componenets/Voting/index'
+import Results from './src/component/Results/Results'
+import Registration from './src/component/Registration/Registration'
+import AddCandidate from './src/componenets/Admin/AddCandidate/index'
+import Verification from './src/componenets/Admin/Verification/index'
+import Footer from './src/componenets/Footer/Footer'
 
-import Home from "./component/Home";
-
-import Voting from "./component/Voting/Voting";
-import Results from "./component/Results/Results";
-import Registration from "./component/Registration/Registration";
-
-import AddCandidate from "./component/Admin/AddCandidate/AddCandidate";
-import Verification from "./component/Admin/Verification/Verification";
-import test from "./component/test";
-
-import Footer from "./component/Footer/Footer";
-
-import "./App.css";
+import './App.css'
 
 export default class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
+      <div className='App'>
         <Router>
           <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/AddCandidate" component={AddCandidate} />
-            <Route exact path="/Voting" component={Voting} />
-            <Route exact path="/Results" component={Results} />
-            <Route exact path="/Registration" component={Registration} />
-            <Route exact path="/Verification" component={Verification} />
-            <Route exact path="/test" component={test} />
-            <Route exact path="*" component={NotFound} />
+            <Route exact path='/' component={Home} />
+            <Route exact path='/AddCandidate' component={AddCandidate} />
+            <Route exact path='/Voting' component={Voting} />
+            <Route exact path='/Results' component={Results} />
+            <Route exact path='/Registration' component={Registration} />
+            <Route exact path='/Verification' component={Verification} />
+            <Route exact path='*' component={NotFound} />
           </Switch>
         </Router>
         <Footer />
       </div>
-    );
+    )
   }
 }
 class NotFound extends Component {
-  render() {
+  render () {
     return (
       <>
         <h1>404 NOT FOUND!</h1>
@@ -45,16 +39,16 @@ class NotFound extends Component {
           <p>
             The page your are looking for doesn't exist.
             <br />
-            Go to{" "}
+            Go to{' '}
             <Link
-              to="/"
-              style={{ color: "black", textDecoration: "underline" }}
+              to='/'
+              style={{ color: 'black', textDecoration: 'underline' }}
             >
               Home
             </Link>
           </p>
         </center>
       </>
-    );
+    )
   }
 }
